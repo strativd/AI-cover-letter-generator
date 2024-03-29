1d2769384cd8f4651db78647fe7231584ee66bd6
strativd <strat.barrett@gmail.com>
2024-03-29 12:12:41 -0700

    Add config for each URL with unique selectors

--- public/background.js
+++ public/background.js
@@ -1,44 +1,55 @@
 /*global chrome*/
 
-// Declaring constants to store LinkedIn URLs.
-const linkedInListViewURL = "https://www.linkedin.com/jobs/collections";
-const linkedInDetailView = "https://www.linkedin.com/jobs/view";
-
-// This function returns the appropriate job description class name based on the URL.
-function getJobDescriptionClassName(url) {
-  // If the URL starts with the LinkedIn list view URL, return the list view class name, else return the detail view class name.
-  return url.startsWith(linkedInListViewURL)
-    ? "jobs-search__job-details--container"
-    : "jobs-description-content__text";
-}
-
-// This function grabs the job description text from the web page.
-function grabJobDescription(className) {
-  const jobDetailsContainer = document.body.querySelector(`.${className}`);
-  const jobDetails = jobDetailsContainer.textContent;
-  const cleanedJobDetails = jobDetails.replace(/\s\s+/g, " ");
-  return cleanedJobDetails;
-}
+const URLS = {
+  "https://www.linkedin.com/jobs/collections": {
+    selector: ".jobs-search__job-details--container",
+  },
+  "https://www.linkedin.com/jobs/view": {
+    selector: ".jobs-description .jobs-description-content__text",
+  },
+};
 
 // This is an event listener that runs when a tab is updated in Chrome.
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // Check if the tab is fully loaded and active.
-  if (changeInfo.status === "complete" && tab.active) {
-    // Check if the URL of the tab matches the LinkedIn list or detail view URL.
-    if (
-      tab.url?.startsWith(linkedInListViewURL) ||
-      tab.url?.startsWith(linkedInDetailView)
-    ) {
-      // Execute the grabJobDescription function on the current tab and store the result in local storage.
-      chrome.scripting
-        .executeScript({
-          target: { tabId: tabId },
-          func: grabJobDescription,
-          args: [getJobDescriptionClassName(tab.url)],
-        })
-        .then((queryResult) => {
-          chrome.storage.local.set({ jobDescription: queryResult[0].result });
-        });
-    }
+  if (!tab.active || !tab.url || changeInfo.status !== "complete") return;
+
+  const validUrl = Object.keys(URLS).find((urlKey) =>
+    tab.url.startsWith(urlKey)
+  );
+  console.log({ changeInfo, validUrl, tab });
+
+  if (validUrl) {
+    // Execute the script on the current tab and store the result in local storage.
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        func: getJobDescription,
+        args: [URLS[validUrl].selector],
+      })
+      .then((queryResult) => {
+        chrome.storage.local.set({ jobDescription: queryResult[0].result });
+        chrome.storage.local.set({ jobTitle: tab.title });
+      });
   }
 });
+
+/** This function is executed on the tab and gets the job description from the web page. */
+function getJobDescription(selector) {
+  const container = document.body.querySelector(selector);
+  const innerText = container.innerText;
+  return innerText.replace(/\s\s+/g, " ");
+}
+
+/** Helper function to get a valid URL object */
+function getValidUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return null;
+  }
+
+  return url;
+}
